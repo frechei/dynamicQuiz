cca5ff8ca5b1b3a80a7ea9859b66c28f407af7a3
frechei <1490409009@qq.com>
2015-09-08 10:10:40 +0800

    debug the grade

--- scripts/main.js
+++ scripts/main.js
@@ -4,27 +4,27 @@
 $(document).ready(function(){
     //用数组将问题、选项和答案存起来
     var allQuestions = [
-           {
-                question: "Who is Prime Minister of the United Kingdom?",
-                choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"],
-                correctAnswer:0
-            },
-           {
-                question: "1973年的动作电影《囚禁》的主演是谁？",
-                choices: ["Amitabh Bachchan", "Danny Boyle", "Dev Patel", "Jamal Malik"],
-                correctAnswer:0
-           },
-           {
-                question: "印度国徽中有三只狮子，狮子下面写的是什么字？",
-                choices: ["The truth alone triumphs真理能战胜一切", "Lies alone triumph谎言能战胜一切",
-                            "Fashion alone triumphs流行能战胜一切", "Money alone triumphs金钱能战胜一切"],
-                correctAnswer:0
-           },
-           {
-                question: "教义中描述的罗摩神右手里握着的是什么？",
-                 choices: ["A flower一朵花", "A sword一把剑", "A child一个小孩", "A bow and a arrow一副弓箭"],
-                correctAnswer:3
-           }];
+        {
+            question: "Who is Prime Minister of the United Kingdom?",
+            choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"],
+            correctAnswer:0
+        },
+        {
+            question: "1973年的动作电影《囚禁》的主演是谁？",
+            choices: ["Amitabh Bachchan", "Danny Boyle", "Dev Patel", "Jamal Malik"],
+            correctAnswer:0
+        },
+        {
+            question: "印度国徽中有三只狮子，狮子下面写的是什么字？",
+            choices: ["The truth alone triumphs真理能战胜一切", "Lies alone triumph谎言能战胜一切",
+                "Fashion alone triumphs流行能战胜一切", "Money alone triumphs金钱能战胜一切"],
+            correctAnswer:0
+        },
+        {
+            question: "教义中描述的罗摩神右手里握着的是什么？",
+            choices: ["A flower一朵花", "A sword一把剑", "A child一个小孩", "A bow and a arrow一副弓箭"],
+            correctAnswer:3
+        }];
 
     //allQuestions第item项
     var item = 0;
@@ -32,15 +32,17 @@ $(document).ready(function(){
     var grade = 0;
     //正确答案
     var correct = -1;
-    $('button').click(function(){
+    $('button').click(function(event){
+        event.preventDefault();
         var html = '';
-
+        //加分项放错位置，导致最后一个不能加分
+        var choose = $('input[name="answer"]:checked').val();
+        if (choose == correct){
+            grade += 25;
+        }
         //每点击一下，就动态地添加下个问题和从屏幕移除目前的问题
         if(item < allQuestions.length){
-            var choose = $('input[name="answer"]:checked').val();
-            if (choose == correct){
-                grade += 25;
-            }
+
             //显示问题和选项
             html += '<p>问题：' + allQuestions[item].question + '</p>';
             for (var i = 0; i < allQuestions[item].choices.length; i++){
@@ -61,5 +63,3 @@ $(document).ready(function(){
         item++;
     });
 });
-
-
