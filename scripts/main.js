166315dba651177c97b03f36344dc627b31831f4
frechei <1490409009@qq.com>
2016-05-21 21:49:29 +0800

    改为使用JSON格式，bootstrap改变模样

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,9 @@
 /**
  * Created by felix on 15-9-7.
  */
-$(document).ready(function(){
-    //用数组将问题、选项和答案存起来
-    var allQuestions = [
-        {
-            question: "Who is Prime Minister of the United Kingdom?",
-            choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"],
-            correctAnswer:0
-        },
-        {
-            question: "1973年的动作电影《囚禁》的主演是谁？",
-            choices: ["Amitabh Bachchan", "Danny Boyle", "Dev Patel", "Jamal Malik"],
-            correctAnswer:0
-        },
-        {
-            question: "印度国徽中有三只狮子，狮子下面写的是什么字？",
-            choices: ["The truth alone triumphs真理能战胜一切", "Lies alone triumph谎言能战胜一切",
-                "Fashion alone triumphs流行能战胜一切", "Money alone triumphs金钱能战胜一切"],
-            correctAnswer:0
-        },
-        {
-            question: "教义中描述的罗摩神右手里握着的是什么？",
-            choices: ["A flower一朵花", "A sword一把剑", "A child一个小孩", "A bow and a arrow一副弓箭"],
-            correctAnswer:3
-        }];
+(function($) {
 
+    var allQuestions = [];
     //allQuestions第item项
     var item = $('p').data("item") || 0;
     //得分
@@ -37,8 +15,12 @@ $(document).ready(function(){
 
     var $button = $('button');
 
+    $.getJSON('choice.json', function(json) {
+            allQuestions = json;
+    });
     var loadHtml = function(item) {
         var html = "";
+        html += '<div class="radio">';
         html += '<p data-item="'+ item + '">问题' + (item+1) + '：' + allQuestions[item].question + '</p>';
         for (var i = 0; i < allQuestions[item].choices.length; i++) {
             html += '<input type="radio" name="answer" value="' + i + '" ';
@@ -47,6 +29,7 @@ $(document).ready(function(){
                 }
             html += ' />'  + allQuestions[item].choices[i] + '<br>';
         } 
+        html += '</div>';
         return html;
     };
 
@@ -129,4 +112,4 @@ $(document).ready(function(){
         var html = '总分：' + grade + '分！'
         $('#quiz').html(html);
     });
-});
+})(jQuery);
